6640e305c9cfe35e9b18bb0464c04fe84111661d
Julio-Guevara <35203056+Julio-Guevara@users.noreply.github.com>
2018-03-31 16:57:20 -0600

    prueba XD

--- app/src/main/java/julio/salazar/guevara/imc/MainActivity.java
+++ app/src/main/java/julio/salazar/guevara/imc/MainActivity.java
@@ -51,6 +51,7 @@ public class MainActivity extends AppCompatActivity
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
+
         }
     }
 
